1038423e6ac1e472b6354866ebc8df52d9b6e507
Nicole Johnson <johsnonbadgersnic222@gmail.com>
2018-05-07 22:06:31 -0400

    lala

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,33 +2,67 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "bamazon_DB"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon_DB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     start();
-  });
+});
 
-  function start() {
+function readProducts() {
+    console.log("Displaying all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
+
+// function to ask customer 
+function askCustomer() {
+    // prompt for info about the item being bought
     inquirer
-      .prompt({
-        name: "postOrBid",
-        type: "rawlist",
-        message: "Would you like to [POST] an auction or [BID] on an auction?",
-        choices: ["POST", "BID"]
-      })
-      .then(function(answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.postOrBid.toUpperCase() === "POST") {
-          postAuction();
+        .prompt([
+            {
+                product_name: "apple",
+                type: "input",
+                message: "What is the id of the product you want to buy?"
+            },
+            {
+                stock_quantity: "5",
+                type: "input",
+                message: "How many units do you want to buy?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                    console.log("Insufficient quantity!");
+                }
+            },
+        ]);
+
+}
+
+function deleteProduct() {
+    console.log("Deleting 5 apples...\n");
+    connection.query(
+        "DELETE FROM products WHERE ?",
+        {
+            product_name: "",
+            stock_quantity: ""
+        },
+        function (err, res) {
+            console.log(res.affectedRows + " products deleted!\n");
+            // Call readProducts AFTER the DELETE completes
+            readProducts();
         }
-        else {
-          bidAuction();
-        }
-      });
-  }
+    );
+}
+
+
